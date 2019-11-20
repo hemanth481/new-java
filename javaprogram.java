085e7c2988c0333bacdf53678f484c5327937582
hemanth481 <57932112+hemanth481@users.noreply.github.com>
2019-11-20 17:57:04 +0530

    Update javaprogram.java

--- javaprogram.java
+++ javaprogram.java
@@ -1,5 +1,7 @@
-class Simple{  
-public static void main(String args[]){  
-  System.out.println(“Hellow World”);  
-    }  
+class Simple
+{  
+public static void main(String args[])
+{  
+  System.out.println(“Hellow World”);      
+}  
 }  
